8cd45b8adfb451a43bf00b888f9b0809a53c954c
Yehia <ahmedyehia611@gmail.com>
2024-08-09 01:26:28 +0300

    tests: book controller tests (all routes)

--- src/test/java/com/lib/api/Book/BookControllerTest.java
+++ src/test/java/com/lib/api/Book/BookControllerTest.java
@@ -50,5 +50,70 @@ class BookControllerTest {
         verify(bookService, times(1)).getBooks();
     }
 
+    @Test
+    void getBookById_ReturnsBook_WhenBookExists() throws Exception {
+        Book book = books.get(0);
+        when(bookService.getBookById(1)).thenReturn(book);
+
+        mockMvc.perform(get("/api/books/{id}", 1))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.title").value(book.title()))
+                .andExpect(jsonPath("$.author").value(book.author()));
+
+        verify(bookService, times(1)).getBookById(1);
+    }
+
+    @Test
+    void getBookById_ReturnsNotFound_WhenBookDoesNotExist() throws Exception {
+        when(bookService.getBookById(1)).thenThrow(new BookNotFoundException());
+
+        mockMvc.perform(get("/api/books/{id}", 1))
+                .andExpect(status().isNotFound());
+
+        verify(bookService, times(1)).getBookById(1);
+    }
+
+    @Test
+    void addBook_CreatesNewBook() throws Exception {
+        Book newBook = new Book("1984", "George Orwell", "9780451524935", 1949, 4);
+
+        mockMvc.perform(post("/api/books")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newBook)))
+                .andExpect(status().isCreated());
+
+        verify(bookService, times(1)).addBook(any(Book.class));
+    }
+
+    @Test
+    void updateBook_UpdatesExistingBook() throws Exception {
+        Book updatedBook = new Book("1984", "George Orwell", "9780451524935", 1949, 5);
+
+        mockMvc.perform(put("/api/books/{id}", 1)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updatedBook)))
+                .andExpect(status().isNoContent());
+
+        verify(bookService, times(1)).updateBook(any(Book.class), eq(1));
+    }
+
+    @Test
+    void deleteBook_DeletesBook_WhenBookExists() throws Exception {
+        mockMvc.perform(delete("/api/books/{id}", 1))
+                .andExpect(status().isNoContent());
+
+        verify(bookService, times(1)).deleteBook(1);
+    }
+
+    @Test
+    void deleteBook_ReturnsNotFound_WhenBookDoesNotExist() throws Exception {
+        doThrow(new BookNotFoundException()).when(bookService).deleteBook(1);
+
+        mockMvc.perform(delete("/api/books/{id}", 1))
+                .andExpect(status().isNotFound());
+
+        verify(bookService, times(1)).deleteBook(1);
+    }
 
 }
